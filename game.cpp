c8dd038de6399717ce9a51870dbda52042caa693
Tejajaladi <83300427+Tejajaladi@users.noreply.github.com>
2021-04-27 21:56:30 +0530

    Add files via upload

---
+++ game.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main (){
+	int number;
+	printf("Enter your number:");
+	scanf("%d",&number);
+	if(number==3)
+	{
+	printf("you won the game");
+	}
+	else if(number==4)
+	{
+	printf("you won the game ");
+	}
+	else if(number==5)
+	{
+	printf("you won the game");
+	}
+	else 
+	{
+		printf("you loose the game");
+	}
+}
